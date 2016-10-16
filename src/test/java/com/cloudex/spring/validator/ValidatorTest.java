18434062e23a7ea856ebbafa167588bae47b4150
changedi <jysoftware@gmail.com>
2016-10-16 15:44:39 +0800

    validator

--- src/test/java/com/cloudex/spring/validator/ValidatorTest.java
+++ src/test/java/com/cloudex/spring/validator/ValidatorTest.java
@@ -1,7 +1,11 @@
 package com.cloudex.spring.validator;
 
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValue;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
 
 /**
  * Created by zunyuan.jy on 16/10/15.
@@ -14,6 +18,22 @@ public class ValidatorTest {
         System.out.println("-------------------------------------------");
 
         TestPersonValidator testPersonValidator = ctx.getBean("testPersonValidator", TestPersonValidator.class);
+        TestPerson testPerson = new TestPerson();
+        DataBinder binder = new DataBinder(testPerson);
+        binder.setValidator(new TestPersonValidator());
 
+        PropertyValue propertyValue = new PropertyValue("name", "Jim");
+        MutablePropertyValues mutablePropertyValues = new MutablePropertyValues();
+        mutablePropertyValues.addPropertyValue(propertyValue);
+        mutablePropertyValues.add("age", -1);
+// bind to the target object
+        binder.bind(mutablePropertyValues);
+
+// validate the target object
+        binder.validate();
+
+// get BindingResult that includes any validation errors
+        BindingResult results = binder.getBindingResult();
+        System.out.println(results);
     }
 }
