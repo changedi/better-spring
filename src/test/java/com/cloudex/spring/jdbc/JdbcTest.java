3f0c72c8da5e2f89713daeaa91cfe71814b8355e
changedi <jysoftware@gmail.com>
2016-11-16 10:04:50 +0800

    update

--- src/test/java/com/cloudex/spring/jdbc/JdbcTest.java
+++ src/test/java/com/cloudex/spring/jdbc/JdbcTest.java
@@ -1,9 +1,13 @@
 package com.cloudex.spring.jdbc;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Created by zunyuan.jy on 16/11/5.
  */
 public class JdbcTest {
+
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
 
