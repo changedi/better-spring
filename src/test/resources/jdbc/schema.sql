8cc7dc4cc2b33080d52d46ce4f7975202ece91c7
changedi <jysoftware@gmail.com>
2016-11-18 17:40:25 +0800

    update

---
+++ src/test/resources/jdbc/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE jdbc_test IF EXISTS;;
+
+
+CREATE TABLE jdbc_test (
+  id         INTEGER IDENTITY PRIMARY KEY,
+  name VARCHAR(30)
+)
---
+++ src/test/resources/jdbc/user_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO jdbc_test VALUES (1, 'James' );
+INSERT INTO jdbc_test VALUES (2, 'Helen');
+INSERT INTO jdbc_test VALUES (3, 'Linda');
+INSERT INTO jdbc_test VALUES (4, 'Rafael');
+INSERT INTO jdbc_test VALUES (5, 'Henry');
+INSERT INTO jdbc_test VALUES (6, 'Sharon');
