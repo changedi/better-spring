c8efc8e8ff525b4e96e91adfdc7b06da9b54d0c4
changedi <jysoftware@gmail.com>
2016-11-29 10:08:12 +0800

    update

---
+++ src/test/java/com/cloudex/spring/oxm/Application.java
@@ -0,0 +1,68 @@
+package com.cloudex.spring.oxm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.Unmarshaller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Created by zunyuan.jy on 16/11/28.
+ */
+public class Application {
+
+    private static final String FILE_NAME = "settings.xml";
+    private Settings settings = new Settings();
+    private Marshaller marshaller;
+    private Unmarshaller unmarshaller;
+
+    public static void main(String[] args) throws IOException {
+        ApplicationContext appContext =
+                new ClassPathXmlApplicationContext("classpath:spring/spring-oxm.xml");
+        Application application = (Application) appContext.getBean("application");
+        application.saveSettings();
+        application.loadSettings();
+
+    }
+
+    public void setMarshaller(Marshaller marshaller) {
+        this.marshaller = marshaller;
+    }
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+        this.unmarshaller = unmarshaller;
+    }
+
+    public void saveSettings() throws IOException {
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(FILE_NAME);
+            this.marshaller.marshal(settings, new StreamResult(os));
+            System.out.println(this.settings.isFooEnabled());
+        } finally {
+            if (os != null) {
+                os.close();
+            }
+        }
+    }
+
+    public void loadSettings() throws IOException {
+        FileInputStream is = null;
+        try {
+            is = new FileInputStream(FILE_NAME);
+            this.settings = (Settings) this.unmarshaller.unmarshal(new StreamSource(is));
+            System.out.println(this.settings.isFooEnabled());
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/cloudex/spring/oxm/Settings.java
@@ -0,0 +1,17 @@
+package com.cloudex.spring.oxm;
+
+/**
+ * Created by zunyuan.jy on 16/11/28.
+ */
+public class Settings {
+
+    private boolean fooEnabled;
+
+    public boolean isFooEnabled() {
+        return fooEnabled;
+    }
+
+    public void setFooEnabled(boolean fooEnabled) {
+        this.fooEnabled = fooEnabled;
+    }
+}
